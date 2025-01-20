54d475fedb070b54145925c90a4dd98d6a45c235
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-20 00:35:17 +0000

    refactor(ActionPayload): use the `Payload` and add the `status` of the action.

--- src/lib/action-payload.interface.ts
+++ src/lib/action-payload.interface.ts
@@ -1,10 +1,17 @@
+// Interface.
+import { Payload } from "./payload.interface";
 /**
- * @description Payload for a specific action (e.g., updating or adding a user).
+ * @description Payload for a specific action (e.g., updating or adding a user). Provides the data associated with performing the action.
  * @export
  * @interface ActionPayload
  * @template [Data=unknown] The type of data related to the action.
+ * @template {string} [Status=string] 
+ * @extends {Payload<Data>}
  */
-export interface ActionPayload<Data = unknown> {
+export interface ActionPayload<
+  Data = unknown,
+  Status extends string = string
+> extends Payload<Data> {
   /**
    * @description The data related to the action (e.g., user data, form data).
    * @type {Data}
@@ -16,4 +23,10 @@ export interface ActionPayload<Data = unknown> {
    * @type {?(string | number)}
    */
   id?: string | number;
+
+  /**
+   * @description A descriptive status of the error (e.g., "failed", "pending", "success").
+   * @type {Status}
+   */
+  status: Status;
 }
