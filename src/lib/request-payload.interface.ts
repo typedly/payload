bce8211a28c095957c4fad33d5623eb104577e56
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-20 00:34:45 +0000

    refactor(RequestPayload): use the `Payload`.

--- src/lib/request-payload.interface.ts
+++ src/lib/request-payload.interface.ts
@@ -1,10 +1,12 @@
+// Interface.
+import { Payload } from "./payload.interface";
 /**
  * @description Represents the type of payload for the requests.
  * @export
  * @interface RequestPayload
  * @template [Data=unknown] The type of data associated with the request.
  */
-export interface RequestPayload<Data = unknown> {
+export interface RequestPayload<Data = unknown> extends Payload<Data> {
   /**
    * @description The data associated with the request (e.g., user data, form data).
    * @type {Data}
